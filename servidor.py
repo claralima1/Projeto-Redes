26f930b34cd2f77a0d5aebc6dce6c8093264321d
Clara Lima <86741485+claralima1@users.noreply.github.com>
2025-02-18 21:01:26 +0000

    teste

--- servidor.py
+++ servidor.py
@@ -1,28 +1,28 @@
 
 import socket
 import json
-from statistics import mean
+from statistics import mean #importa do módulo a função "mean" para calcular a media dos dados
 
 class Servidor:
     def __init__(self, endereco, porta):
         self.endereco = endereco
         self.porta = porta
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.endereco, self.porta))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Cria um socket (AF_INET = IPv4(protocolo utilizado)) (SOCK_STREAM = define o tipo de socket (TCP))
+        self.socket.bind((self.endereco, self.porta)) #função para associar um socket a um endereço IP e porta que o servidor estará usando para ouvir conexões
 
-    def escutar(self):
-        self.socket.listen(5)
+    def escutar(self): #Função que faz o servidor começar a escutar conexões de clientes
+        self.socket.listen(5) #5 é o número de conexões que o servidor pode ter na fila
         print(f"Servidor ouvindo na porta {self.porta}...")
 
-        while True:
-            socket_cliente, endereco_cliente = self.socket.accept()
+        while True: #loop para esperar por novas conexões
+            socket_cliente, endereco_cliente = self.socket.accept() #aguarda a conexão de um cliente | Quando um cliente se conecta, "accept()" retorna dois valores(socket_cliente e  endereco_cliente)
             print(f"Conexão recebida de {endereco_cliente}")
 
             # Recebe os dados do cliente
-            dados = socket_cliente.recv(1024)
-            if dados:
-                self.processar_dados(dados, socket_cliente)
-            socket_cliente.close()
+            dados = socket_cliente.recv(1024) #lê até 1024 bytes (amanho de buffer)
+            if dados: #se o cliente enviar algo
+                self.processar_dados(dados, socket_cliente) #processa as informações recebidas
+            socket_cliente.close() #Depois de processar os dados, a conexão com o cliente é fechada (para liberar os recursos do sitema)
 
     def processar_dados(self, dados, socket_cliente):
         """ Processa os dados recebidos do cliente """
